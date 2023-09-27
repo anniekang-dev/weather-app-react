61868b37dc5e979dcfcc300c1a1cdec36c9fa287
anniekang-dev <hyeju0722@gmail.com>
2023-09-26 21:50:18 -0500

    API call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import searchIcon from './assets/icons-search.png';
@@ -8,15 +8,31 @@ import airIcon from './assets/blur.png';
 import humidityIcon from './assets/humidity.png';
 import windIcon from './assets/wind.png';
 
-function App() {
+export default function App() {
+  const [ data, setData ] = useState([])
+  const [ location, setLocation ] = useState('')
 
-  // const url ='https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=dc3e1bfc785b02ec2d95cb7236385598';
+  const API_KEY = 'dc3e1bfc785b02ec2d95cb7236385598';
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+  const searchLocation = async (e) => {
+    if(e.key === 'Enter') {
+       {
+        const data = await axios({
+          method: 'get',
+          url: url
+        })
+        console.log(data);
+      }
+    }
+  }
+
 
   return (
     <div className="app">
       <div className="container">
         <div className="search-bar">
-          <input type="text" placeholder="Enter location" />
+          <input type="text" value={location} onChange={event => setLocation(event.target.value)} onKeyDown={searchLocation}placeholder="Enter location" />
           <div className="search-icon">
             {/* <img src={searchIcon} alt=""/> */}
           </div>
@@ -29,7 +45,7 @@ function App() {
             <p>88</p><p id='degree'>ºF</p>
           </div>
           <div className="location">
-            <p>Dallas</p>
+            <p>{data.name}</p>
           </div>
           <div className="description">
             <p>Clouds</p>
@@ -59,5 +75,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
